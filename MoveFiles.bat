71aa5c9c1aa92fec5e27bb0eec94f71982b9f1ac
Heep <contact.heep@gmail.com>
2015-08-22 12:02:20 +0100

    Update MoveFiles.bat

--- MoveFiles.bat
+++ MoveFiles.bat
@@ -1,1 +1,8 @@
+@echo off
+title Heepsbot: Moving Scripts
+echo Press any key to move scripts to new location.
+pause >nul
 move /-y "%HOMEPATH%\Downloads\Heepsbot\Remotes\*.ini" "%APPDATA%\mIRC\scripts\Remotes"
+echo Scripts have been moved. Press any key to close this window.
+pause >nul
+exit
